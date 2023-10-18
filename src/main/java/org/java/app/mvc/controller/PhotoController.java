9289445d8f992f59d6a6789d7dec384d45f49709
mirkettinho <mirkodifranco30@gmail.com>
2023-10-18 20:37:39 +0200

    added show

--- src/main/java/org/java/app/mvc/controller/PhotoController.java
+++ src/main/java/org/java/app/mvc/controller/PhotoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -25,5 +26,15 @@ public class PhotoController {
 		model.addAttribute("searchInput", searchInput);
 		
 		return "photos/index";
+	}
+	
+	@GetMapping("/photos/{id}")
+	public String getShow(@PathVariable Integer id, Model model) {
+		
+		Photo photo = photoServ.findById(id);
+		model.addAttribute("photo", photo);
+		
+		return "/photos/show";
+		
 	}
 }
